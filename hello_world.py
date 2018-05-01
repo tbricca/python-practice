ad315802314b4bb9396ea8bac3efc255722c78fe
tbricca <tbricca@gmail.com>
2018-05-01 09:54:59 -0700

    added notes on variables and data types

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,40 @@
 print("Hello World!")
-print("Goodbye World!")
+print("Goodbye World!") #this is how comments are in python 
 
-print(1+2)
+########## NUMBERS & MATH ###############
+
+#division returns float numbers
+# NEW MATH OPERATORS
+
+# ** = Exponentiation  raises it to a power
+print (2 ** 3) 
+# = 8 
+
+# % = Modulo (remainder operator)
+# ----- it takes 2 numbers, takes one of them and divides it into it as many times as possible and then gives us the remainder
+print (10 % 3) 
+# = 1 
+
+# = Integer Division 
+# divides and answers it without a float or decimal 
+print(10//3)
+# = 3
+
+######## VARIABLES ############
+# They're like contianers - they store some data and you can pull it out later 
+# They hold a value and have a name 
+# ex. of things you can store in a variable - numbers, booleans & strings 
+
+### Variables can be resassinged 
+# python = 100
+# another_var = python 
+# python = 1337 
+# Assigning multiple variables at once - all, at, once = 5, 10, 15 
+# all = 5; at = 10; once = 15
+
+############# DATA TYPES #############
+# bool - true or false values 
+# int - an integer (1,2,3)
+# str - string - a sequenxe of Unicode characters "____" 
+# list - an ordered sequence of values of other data types e.g [1,2,3] or ["a","b", "c"]
+# dict - a collection of key:vaules, e.g.{"first_name": "Colt", "last_name": "Steele"}
